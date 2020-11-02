909702858b1f1713fd0fb7054a973d302cdf7f5b
AnttiHalmetoja <anttihalmetoja@gmail.com>
2020-11-02 20:37:52 +0200

    Added laskarit, viikko 2 directory and maksukortti

---
+++ laskarit/viikko2/Maksukortti/src/test/java/MaksukorttiTest.java
@@ -0,0 +1,50 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MaksukorttiTest {
+
+    public MaksukorttiTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void hello() {}
+     @Test
+public void konstruktoriAsettaaSaldonOikein() {
+    Maksukortti kortti = new Maksukortti(10);
+
+    String vastaus = kortti.toString();
+
+    assertEquals("Kortilla on rahaa 10.0 euroa", vastaus);
+}
+@Test
+public void syoEdullisestiVahentaaSaldoaOikein() {
+    Maksukortti kortti = new Maksukortti(10);
+
+    kortti.syoEdullisesti();
+
+    assertEquals("Kortilla on rahaa 7.5 euroa", kortti.toString());
+}
+}
